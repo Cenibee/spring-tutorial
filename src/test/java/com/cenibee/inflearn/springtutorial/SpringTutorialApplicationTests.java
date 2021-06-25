100e7233382bfae95c1338f6834e2408c5f5d19f
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-06-26 01:24:32 +0900

    :tada: 프로젝트 초기화

---
+++ src/test/java/com/cenibee/inflearn/springtutorial/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenibee.inflearn.springtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
